87b1ce45e4eb207e4bbc7ccaa63e0cb8153aa134
Renan A. de M. Rocha <renanrochamorais@outlook.com>
2025-10-28 11:53:03 -0300

    Estrutura de header e footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FooterComponent
+  ],
   templateUrl: './app.html',
 })
 export class App {}
